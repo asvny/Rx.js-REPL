a20ecf0a7b5b3cfb89e46a591b30dfcc3fcca6c0
Annamalai <annamalai-saro@users.noreply.github.com>
2015-09-18 17:48:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ console.log('Welcome to Rx.js REPL !');
 module.exports= function() {
 
 	var repl   =require('repl'),
-		rxREPL =repl.start({ prompt : 'rx > '});
+            rxREPL =repl.start({ prompt : 'rx > '});
 	    require('repl.history')(rxREPL, process.env.HOME + '/.rx_history');
+		
+	    rxREPL.context.rx=rx;
+            rxREPL.context.rxNode=rxNode;
 
-		rxREPL.context.rx=rx;
-		rxREPL.context.rxNode=rxNode;
-
-		Object.keys(rx).map(function(func){
-
-			rxREPL.context[func]=rx[func];
-		})
+	     Object.keys(rx).map(function(func){
+		rxREPL.context[func]=rx[func];
+	     });
 };
 
 
